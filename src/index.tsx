14c30e284f0c082509a4063aec271fc71aafd218
Tyler Martinez <slightlytyler@gmail.com>
2020-07-21 10:27:21 -0700

    Fixes

--- src/index.tsx
+++ src/index.tsx
@@ -3,27 +3,38 @@ import React, {
   useContext,
   useEffect,
   useMemo,
-  useState
+  useState,
+  useRef
 } from 'react';
+import { v4 as generateId } from 'uuid';
 
 // Types
 
 type Fill = any;
 
+type SlotState = {
+  fillsById: {
+    [id: string]: Fill;
+  };
+  fillsOrder: Array<string>;
+};
+
 // Components
 
 type SlotFillContextValue = {
-  fills: {
-    [name: string]: Array<Fill>;
+  slots: {
+    [slotName: string]: SlotState;
   };
-  pushFill: (name: string, fill: Fill) => void;
-  popFill: (name: string) => void;
+  pushFill: (slotName: string, fill: Fill) => string;
+  replaceFill: (slotName: string, id: string, fill: Fill) => void;
+  removeFill: (slotName: string, id: string) => void;
 };
 
 const defaultContextValue = {
-  fills: {},
-  pushFill: () => {},
-  popFill: () => {}
+  slots: {},
+  pushFill: () => '',
+  replaceFill: () => {},
+  removeFill: () => {}
 } as SlotFillContextValue;
 
 const SlotFillContext = React.createContext(defaultContextValue);
@@ -34,36 +45,63 @@ type SlotFillProviderProps = {
 
 export function SlotFillProvider(props: SlotFillProviderProps) {
   const { children } = props;
-  const [fills, setFills] = useState<SlotFillContextValue['fills']>({});
-  const pushFill = useCallback(
-    (slotName: string, fill: NonNullable<React.ReactNode>) =>
-      setFills((prevFills) => {
-        const fillsForSlot = prevFills[slotName] || [];
-        const nextFillsForSlot = [...fillsForSlot, fill];
-        return {
-          ...prevFills,
-          [slotName]: nextFillsForSlot
-        };
-      }),
+  const [slots, setSlots] = useState<SlotFillContextValue['slots']>({});
+  const pushFill = useCallback((slotName: string, fill: Fill) => {
+    const id = generateId();
+    setSlots((prevSlots) => {
+      const prevFillsById = prevSlots[slotName]?.fillsById || {};
+      const prevFillsOrder = prevSlots[slotName]?.fillsOrder || [];
+      return {
+        ...prevSlots,
+        [slotName]: {
+          fillsById: {
+            ...prevFillsById,
+            [id]: fill
+          },
+          fillsOrder: [...prevFillsOrder, id]
+        }
+      };
+    });
+    return id;
+  }, []);
+  const replaceFill = useCallback(
+    (slotName: string, id: string, fill: Fill) => {
+      return setSlots((prevSlots) => ({
+        ...prevSlots,
+        [slotName]: {
+          ...prevSlots[slotName],
+          fillsById: {
+            ...prevSlots[slotName].fillsById,
+            [id]: fill
+          }
+        }
+      }));
+    },
     []
   );
-  const popFill = useCallback(
-    (slotName: string) =>
-      setFills((prevFills) => {
-        const fillsForSlot = prevFills[slotName];
-        const nextFillsForSlot = [...fillsForSlot];
-        nextFillsForSlot.pop();
-        return {
-          ...prevFills,
-          [slotName]: nextFillsForSlot
-        };
-      }),
-    []
-  );
-  const value = useMemo(() => ({ fills, pushFill, popFill }), [
-    fills,
+  const removeFill = useCallback((slotName: string, id: string) => {
+    return setSlots((prevSlots) => {
+      const prevFillsById = prevSlots[slotName].fillsById;
+      const prevFillsOrder = prevSlots[slotName].fillsOrder;
+      const index = prevFillsOrder.findIndex((fillId) => id === fillId);
+      const nextFillsById = { ...prevFillsById };
+      delete nextFillsById[id];
+      const nextFillsOrder = [...prevFillsOrder];
+      nextFillsOrder.splice(index, 1);
+      return {
+        ...prevSlots,
+        [slotName]: {
+          fillsById: nextFillsById,
+          fillsOrder: nextFillsOrder
+        }
+      };
+    });
+  }, []);
+  const value = useMemo(() => ({ slots, pushFill, replaceFill, removeFill }), [
+    slots,
     pushFill,
-    popFill
+    replaceFill,
+    removeFill
   ]);
   return (
     <SlotFillContext.Provider value={value}>
@@ -79,22 +117,67 @@ type SlotProps = {
 
 export function Slot(props: SlotProps) {
   const { children, name } = props;
-  const { fills } = useContext(SlotFillContext);
-  const fillsForSlot = fills[name] || [];
-  return children(fillsForSlot);
+  const { slots } = useSlotFill();
+  const slotState = slots[name];
+  if (!slotState) return children([]);
+  const fills = orderedFills(slotState);
+  return children(fills);
 }
 
 type FillProps = {
-  children: NonNullable<React.ReactNode>;
+  children: Fill | ((prevFill: Fill) => Fill);
   name: string;
 };
 
 export function Fill(props: FillProps) {
   const { children, name } = props;
-  const { pushFill, popFill } = useContext(SlotFillContext);
+  const { slots, pushFill, replaceFill, removeFill } = useSlotFill();
+  const idRef = useRef<string>();
+  const output = useMemo(() => {
+    if (typeof children === 'function') {
+      const slotState = slots[name];
+      const index = idRef.current
+        ? slotState?.fillsOrder.findIndex((fillId) => idRef.current === fillId)
+        : undefined;
+      const prevIndex = index ? index - 1 : -1;
+      const prevFillId = slotState?.fillsOrder[prevIndex];
+      const prevFill = slotState?.fillsById[prevFillId];
+      return children(prevFill);
+    }
+    return children;
+  }, [children, name, slots]);
   useEffect(() => {
-    pushFill(name, children);
-    return () => popFill(name);
-  }, [children, name]);
+    const id = idRef.current;
+    if (id) {
+      replaceFill(name, id, output);
+    } else {
+      const newId = pushFill(name, output);
+      idRef.current = newId;
+    }
+  }, [output, name]);
+  useEffect(() => {
+    return () => removeFill(name, idRef.current!);
+  }, []);
   return null;
+}
+
+// Hooks
+
+export function useSlotFill() {
+  return useContext(SlotFillContext);
+}
+
+export function useFills(slotName: string) {
+  const slots = useSlotFill().slots;
+  const slotState = slots[slotName];
+  if (!slotState) return [];
+  const fills = orderedFills(slotState);
+  return fills;
+}
+
+// Helpers
+
+function orderedFills(slotState: SlotState) {
+  const { fillsById, fillsOrder } = slotState;
+  return fillsOrder.map((id) => fillsById[id]);
 }
